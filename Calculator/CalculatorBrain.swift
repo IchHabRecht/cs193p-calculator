12f2f48cf033907f5878a6bb11ee6323dd829e42
Nicole Cordes <typo3@cordes.co>
2016-11-04 18:03:12 +0100

    Add basic CalculatorBrain API

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,29 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Nicole on 04.11.16.
+//  Copyright © 2016 IchHabRecht Inc. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain {
+    
+    // Read-only property for operation result
+    var result: Double {
+        get {
+            return 0.0
+        }
+    }
+    
+    // Sets the accumulator
+    func setOperand(operand: Double) {
+    }
+    
+    // Performs the operation and stores the result
+    func performOperation (symbol: String) {
+        
+    }
+    
+}
